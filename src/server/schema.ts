6b7d0805a5398770d9658a23b3626c9fed6cae45
mohdasaadeh <mass.1993@hotmail.com>
2024-04-09 18:39:49 +0300

    fixing some bugs in turso setup

--- src/server/schema.ts
+++ src/server/schema.ts
@@ -2,6 +2,6 @@ import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
   id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-  username: text("text"),
-  password: text("text"),
+  username: text("username"),
+  password: text("password"),
 });
